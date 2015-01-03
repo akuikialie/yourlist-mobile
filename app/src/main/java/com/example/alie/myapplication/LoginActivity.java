f64172e92fd07ee45db90f5f34ccdf342bdb8a35
Akku Iki Alie <ali@firzil.co.id>
2015-01-03 12:47:20 +0700

    Layout : add menu home

--- app/src/main/java/com/example/alie/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/alie/myapplication/LoginActivity.java
@@ -1,27 +1,45 @@
 package com.example.alie.myapplication;
 
-import android.app.ProgressDialog;
+import android.app.Activity;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.app.Activity;
-import android.graphics.Color;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends ActionBarActivity {
+public class LoginActivity extends Activity {
+    private EditText  username=null;
+    private EditText  password=null;
+    private Button login;
+    Context c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.mylogin);
+        username = (EditText)findViewById(R.id.textUsername);
+        password = (EditText)findViewById(R.id.textPassword);
+        login = (Button)findViewById(R.id.buttonSubmitLogin);
+    }
+
+    public void loginAction(View view){
+        if(username.getText().toString().equals("akuikialie") &&
+            password.getText().toString().equals("admin")){
+                Toast.makeText(getApplicationContext(), "Redirecting...",
+                Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+
+        else{
+            Toast.makeText(getApplicationContext(), "Wrong Credentials",
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
