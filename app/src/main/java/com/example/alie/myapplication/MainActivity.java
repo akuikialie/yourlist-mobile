8e6cd12d1a0adefd7afd30b180af700650004ba7
Akku Iki Alie <ali@firzil.co.id>
2015-01-03 12:33:13 +0700

    Layout : fix with fullscreet n no titlebar

--- app/src/main/java/com/example/alie/myapplication/MainActivity.java
+++ app/src/main/java/com/example/alie/myapplication/MainActivity.java
@@ -1,19 +1,53 @@
 package com.example.alie.myapplication;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
+    private EditText  username=null;
+    private EditText  password=null;
+    private Button login;
+    Context c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.mylogin);
+        username = (EditText)findViewById(R.id.textUsername);
+        password = (EditText)findViewById(R.id.textPassword);
+        login = (Button)findViewById(R.id.buttonSubmitLogin);
     }
 
+    public void loginAction(View view){
+        if(username.getText().toString().equals("akuikialie") &&
+            password.getText().toString().equals("admin")){
+                Toast.makeText(getApplicationContext(), "Redirecting...",
+                Toast.LENGTH_SHORT).show();
+            }
+        else{
+            Toast.makeText(getApplicationContext(), "Wrong Credentials",
+                    Toast.LENGTH_SHORT).show();
+            /*attempts.setBackgroundColor(Color.RED);
+            counter--;
+            attempts.setText(Integer.toString(counter));
+            if(counter==0){
+                login.setEnabled(false);
+            }*/
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
